1299902f72c5f0882bba129f0e087e04ca65b974
Maximiliano Arévalo <maximiliano.arevalo@usach.cl>
2019-06-08 22:49:44 -0400

    Agregada funcion para generos

--- backend general/src/main/java/backend/services/GenreService.java
+++ backend general/src/main/java/backend/services/GenreService.java
@@ -2,7 +2,9 @@ package backend.services;
 
 import backend.models.Genre;
 
+import backend.models.GenreStatistic;
 import backend.repositories.GenreRepository;
+import backend.repositories.GenreStatisticRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,7 +14,8 @@ import java.sql.Timestamp;
 
 import java.text.ParseException;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @CrossOrigin
 @RestController
@@ -21,6 +24,8 @@ public class GenreService {
 
     @Autowired
     private GenreRepository genreRepository;
+    @Autowired
+    private GenreStatisticRepository genreStatisticRepository;
 
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
@@ -39,5 +44,72 @@ public class GenreService {
     public Genre create(@RequestBody Genre resource) throws ParseException {
 
         return genreRepository.save(resource);
+    }
+
+    @GetMapping("/valoracion")
+    @ResponseBody
+    public List<GenreStatistic> getAllStatistic(){
+        return genreStatisticRepository.findAll();
+    }
+
+    @GetMapping("/totalPositivoNegativo")
+    @ResponseBody
+    public HashMap<String, Object> getAllSentiment(){
+        HashMap<String, Object> map = new HashMap<>();
+        List<GenreStatistic> data = genreStatisticRepository.findAll();
+        int positive = 0;
+        int negative = 0;
+        for(GenreStatistic genre : data){
+            positive = positive + genre.getPositives();
+            negative = negative + genre.getNegatives();
+        }
+        map.put("positive", positive);
+        map.put("negative", negative);
+        map.put("total", positive + negative);
+        return map;
+    }
+
+    @GetMapping("/sortedGenre")
+    @ResponseBody
+    public HashMap<String, Integer> getSortedByAsc(){
+        HashMap<String, Integer> map = new HashMap<>();
+        List<GenreStatistic> data = genreStatisticRepository.findAll();
+        for(GenreStatistic genre : data){
+            Genre genreData = genreRepository.findGenreById(genre.getGenreId());
+            map.put(genreData.getGenre(), genre.getPositives() + genre.getNegatives());
+        }
+        return sortByValue(map, false);
+    }
+
+    @GetMapping("/getGenreStadistic")
+    @ResponseBody
+    public List<HashMap<String, Object>> getGenreStadistic(){
+        HashMap<String, Object> map = new HashMap<>();
+        List<HashMap<String, Object>> result = new ArrayList<>();
+        List<GenreStatistic> data = genreStatisticRepository.findAll();
+        for(GenreStatistic genre : data){
+            Genre genreData = genreRepository.findGenreById(genre.getGenreId());
+            map.put("genre", genreData.getGenre());
+            map.put("positive", genre.getPositives());
+            map.put("negative", genre.getNegatives());
+            map.put("total", genre.getPositives() + genre.getNegatives());
+            result.add(map);
+            map = new HashMap<>();
+        }
+        return result;
+    }
+
+    private static HashMap<String, Integer> sortByValue(Map<String, Integer> unsortMap, final boolean order)
+    {
+        List<Map.Entry<String, Integer>> list = new LinkedList<>(unsortMap.entrySet());
+
+        // Sorting the list based on values
+        list.sort((o1, o2) -> order ? o1.getValue().compareTo(o2.getValue()) == 0
+                ? o1.getKey().compareTo(o2.getKey())
+                : o1.getValue().compareTo(o2.getValue()) : o2.getValue().compareTo(o1.getValue()) == 0
+                ? o2.getKey().compareTo(o1.getKey())
+                : o2.getValue().compareTo(o1.getValue()));
+        return list.stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> b, LinkedHashMap::new));
+
     }
 }
