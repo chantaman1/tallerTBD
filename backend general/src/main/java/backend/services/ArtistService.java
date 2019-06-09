232fe80060380613d3ad3db91ce025e8aca63c80
Edgar Blau <edgar.blau@usach.cl>
2019-06-08 22:04:42 -0400

    Agregado obtencion de tweets via API.

--- backend general/src/main/java/backend/services/ArtistService.java
+++ backend general/src/main/java/backend/services/ArtistService.java
@@ -2,8 +2,10 @@ package backend.services;
 
 import backend.models.Artist;
 
+import backend.models.ArtistStatistic;
 import backend.repositories.ArtistRepository;
 
+import backend.repositories.ArtistStatisticRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +14,8 @@ import java.sql.Timestamp;
 
 import java.text.ParseException;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @CrossOrigin
 @RestController
@@ -21,6 +24,9 @@ public class ArtistService {
 
     @Autowired
     private ArtistRepository artistRepository;
+
+    @Autowired
+    private ArtistStatisticRepository artistStatisticRepository;
 
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
@@ -40,4 +46,54 @@ public class ArtistService {
 
         return artistRepository.save(resource);
     }
+
+    @GetMapping("/valoracion")
+    @ResponseBody
+    public List<ArtistStatistic> getAllStatistic(){
+        return artistStatisticRepository.findAll();
+    }
+
+    @GetMapping("/totalPositivoNegativo")
+    @ResponseBody
+    public HashMap<String, Object> getAllSentiment(){
+        HashMap<String, Object> map = new HashMap<>();
+        List<ArtistStatistic> data = artistStatisticRepository.findAll();
+        int positive = 0;
+        int negative = 0;
+        for(ArtistStatistic artista : data){
+            positive = positive + artista.getPositives();
+            negative = negative + artista.getNegatives();
+        }
+        map.put("positive", positive);
+        map.put("negative", negative);
+        map.put("total", positive + negative);
+        return map;
+    }
+
+    @GetMapping("/sortedArtist")
+    @ResponseBody
+    public HashMap<String, Integer> getSortedByAsc(){
+        HashMap<String, Integer> map = new HashMap<>();
+        List<ArtistStatistic> data = artistStatisticRepository.findAll();
+        for(ArtistStatistic artist : data){
+            Artist artistData = artistRepository.findArtistById(artist.getArtistId());
+            map.put(artistData.getName(), artist.getPositives() + artist.getNegatives());
+        }
+        return sortByValue(map, false);
+    }
+
+    private static HashMap<String, Integer> sortByValue(Map<String, Integer> unsortMap, final boolean order)
+    {
+        List<Map.Entry<String, Integer>> list = new LinkedList<>(unsortMap.entrySet());
+
+        // Sorting the list based on values
+        list.sort((o1, o2) -> order ? o1.getValue().compareTo(o2.getValue()) == 0
+                ? o1.getKey().compareTo(o2.getKey())
+                : o1.getValue().compareTo(o2.getValue()) : o2.getValue().compareTo(o1.getValue()) == 0
+                ? o2.getKey().compareTo(o1.getKey())
+                : o2.getValue().compareTo(o1.getValue()));
+        return list.stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> b, LinkedHashMap::new));
+
+    }
+
 }
