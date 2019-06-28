e33532be6493726648674014a2969313b6615829
Edgar Blau <edgar.blau@usach.cl>
2019-06-28 17:53:21 -0400

    Servicio de neo4j hecho.

--- backend general/src/main/java/backend/services/Neo4jService.java
+++ backend general/src/main/java/backend/services/Neo4jService.java
@@ -3,21 +3,39 @@ package backend.services;
 import backend.Neo4J.Neo4J;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
-import org.neo4j.drive.v1.Record;
-import org.neo4j.drive.v1.StatementResult;
-import org.neo4j.driver.v1.Value;
 
+import java.util.HashMap;
 import java.util.List;
-import java.util.ArrayList;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/neo4j")
-public class Neo4JService{
-  @Autowired
-  Neo4J neo;
+public class Neo4jService{
+    @Autowired
+    Neo4J neo;
 
-  //Soy un metodo
+    @GetMapping("/artistaPopular")
+    @ResponseBody
+    public List<HashMap<String, Object>> getArtistaPopular(){
+      return neo.obtenerUsuariosArtistaPopular();
+    }
+
+    @GetMapping("/generoPopular")
+    @ResponseBody
+    public List<HashMap<String, Object>> getGeneroPopular(){
+      return neo.obtenerUsuariosGeneroPopular();
+    }
+
+    @GetMapping("/usuarioGenero")
+    @ResponseBody
+    public List<HashMap<String, Object>> getUsuarioGenero(){
+      return neo.obtenerUsuarioGenero();
+    }
+
+    @GetMapping("/usuarioArtista")
+    @ResponseBody
+    public List<HashMap<String, Object>> getUsuarioArtista(){
+      return neo.obtenerUsuarioArtista();
+    }
 }
